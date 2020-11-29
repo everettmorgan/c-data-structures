d13854725aa046d58548e4975ea49dfc7c9bcf5f
ejmorgan <admin@ejmorgan.com>
2020-11-29 08:13:26 -0800

    refactor into function + add PADD check

--- arrays/one.c
+++ arrays/one.c
@@ -1,22 +1,31 @@
 #include<stdio.h>
 #include<limits.h>
 
+#define ARRAY_LEN 15
+#define PADD      -1
+
 // Find the second minimum element of an array
 
-int main() {
-  int e[] = {255,64,34,97,86,36,35,42,913,36};
+int find_sec_low(int a[ARRAY_LEN]) {
+  int flw = INT_MAX; // first lowest
+  int slw = INT_MAX; // second lowest
 
-  int cl = INT_MAX;
-  int nl = INT_MAX;
-
-  for(int i=0; i < 10; i++) {
-    if (e[i] < cl) {
-      nl = cl;
-      cl = e[i];
-    } else if (e[i] < nl) {
-      nl = e[i];
+  for(int i=0; i < ARRAY_LEN; i++) {
+    if (a[i] == PADD) break;
+    if (a[i] < flw) {
+      slw = flw;
+      flw = a[i];
+    } else if (a[i] < slw) {
+      slw = a[i];
     }
   }
 
-  printf("lowest: %d second lowest: %d\n", cl, nl);
+  return slw;
+}
+
+int main() {
+  int e[ARRAY_LEN] = {255,64,34,97,86,36,35,42,913,36,PADD};
+  int slw = find_sec_low(e);
+
+  printf("lowest: %d\n", slw);
 }
