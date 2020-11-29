2f9401cf96dae9106b79aae1cf51f1320fa82483
ejmorgan <admin@ejmorgan.com>
2020-11-29 12:39:29 -0800

    add more tests + move cond into if statement

--- arrays/two.c
+++ arrays/two.c
@@ -8,17 +8,12 @@
 // finds the first non-repeating character or PADD terminal. ff_non_rep() will
 // return -1 if no non-repeating integer is found in the given array.
 // ff_non_rep() expects incoming arrays to terminate themselves with -1 or the
-// PADD symbolic link.
+// PADD symbolic link. Arrays without a PADD termination will result in undefined
+// behavior and incorrect return results.
 int ff_non_rep(int a[]) {
-  int prev, nrep;
+  int prev, nrep = -1;
 
   for (int i=0; a[i] != PADD; i++) {
-    // empty or PADD-only array
-    if (a[i] == PADD) {
-      nrep = -1;
-      break;
-    }
-
     // return array if there's only 1 element
     if (a[i+1] == PADD) {
       nrep = a[i];
@@ -57,18 +52,39 @@ int ff_non_rep(int a[]) {
 }
 
 int main() {
+  // valid
+  int o1[] = {22,-1}; // -1 terminal
+  int o2[] = {22,PADD}; // one
   int d[] = {1,1,2,2,22,PADD}; // even
   int t[] = {1,1,1,2,2,2,22,3,3,PADD}; // odd
   int s[] = {22,1,1,2,2,3,PADD}; // start
   int e[] = {1,1,2,2,3,3,22,PADD}; // end
 
+  int o1nrep = ff_non_rep(o1);
+  int o2nrep = ff_non_rep(o2);
   int dnrep = ff_non_rep(d);
   int tnrep = ff_non_rep(t);
   int snrep = ff_non_rep(s);
   int enrep = ff_non_rep(e);
 
+  printf("== valid ===\n");
+  printf("o1nrep: %d\n", o1nrep);
+  printf("o2nrep: %d\n", o2nrep);
   printf("dnrep: %d\n", dnrep);
   printf("tnrep: %d\n", tnrep);
   printf("snrep: %d\n", snrep);
   printf("enrep: %d\n", enrep);
+
+  printf("\n== invalid ===\n");
+  int e1[] = {PADD}; // padd_only
+  int e2[] = {}; // zero_elem
+  int e3[] = {22}; // no_padd
+
+  int e1res = ff_non_rep(e1);
+  int e2res = ff_non_rep(e2);
+  int e3res = ff_non_rep(e3);
+
+  printf("padd_only: %d\n", e1res);
+  printf("zero_elem: %d\n", e2res);
+  printf("no padd: %d\n", e3res);
 }
