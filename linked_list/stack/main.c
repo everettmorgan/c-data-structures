21dc223a1a580e0e062d83bca184cc8acad6cf50
ejmorgan <admin@ejmorgan.com>
2020-11-29 15:07:56 -0800

    refactor

--- linked_list/stack/main.c
+++ linked_list/stack/main.c
@@ -4,22 +4,24 @@
 
 #include "stack.h"
 
+
+
 int main() {
-  stack s = {};
-  push(&s, 'h');
-  push(&s, 'e');
-  push(&s, 'l');
-  push(&s, 'l');
-  push(&s, 'o');
+  stack *s = new_stack(5);
+  push(s, 'h');
+  push(s, 'e');
+  push(s, 'l');
+  push(s, 'l');
+  push(s, 'o');
 
-  print_stack(&s);
+  print_stack(s);
 
-  pop(&s);
-  pop(&s);
-  pop(&s);
+  pop(s);
+  pop(s);
+  pop(s);
 
-  node *h = peek(&s);
+  node *h = peek(s);
   printf("head: %p\n\n", h);
 
-  print_stack(&s);
+  print_stack(s);
 }
