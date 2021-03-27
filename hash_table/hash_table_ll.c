37e854a728290d6a8892a2f71b6fc4714c038ace
ejmorgan <admin@ejmorgan.com>
2021-03-27 11:28:41 -0700

    refactor

--- hash_table/hash_table_ll.c
+++ hash_table/hash_table_ll.c
@@ -2,43 +2,48 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+#ifndef HASH_TABLE_LL
+#define HASH_TABLE_LL
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+
 #include "../linked_list/linked_list.h"
 
-#define SIZE 25
+#ifndef HASH_TABLE_SIZE
+  #define HASH_TABLE_SIZE 25
+#endif
 
-typedef void * hash_table[SIZE];
+typedef void ** hash_table;
 
 typedef struct hash_tbl_node {
   void * data;
   int key;
 } hnode;
 
-hash_table * hash_table_new() {
-  hash_table * ht = calloc(SIZE, sizeof(void *));
-  return ht;
+hash_table hash_table_new() {
+  return calloc(HASH_TABLE_SIZE, sizeof(void *));
 }
 
-LL_EQ(hnode_key, hnode, int, (_a->key == *_b));
+int hnode_key_equality(hnode * a, int * b) { return a->key == *b; }
 
-void hash_table_insert(hash_table * ht, void * d, int key) {
-    int ll = key % (SIZE-1);
-    if ((*ht)[ll] == NULL) {
-      (*ht)[ll] = linked_list_new(0);
-      linked_list_equality_fn((*ht)[ll], hnode_key_equality);
+void hash_table_insert(hash_table ht, void * d, int key) {
+    int ll = key % (HASH_TABLE_SIZE-1);
+    if (ht[ll] == NULL) {
+      ht[ll] = linked_list_new(0);
+      linked_list_equality_fn(ht[ll], hnode_key_equality);
     }
     hnode * hn = malloc(sizeof(hnode));
     hn->data = d;
     hn->key = key;
-    LL_INSERT((*ht)[ll], hn);
+    LL_INSERT(ht[ll], hn);
 }
 
-hnode * hash_table_search(hash_table * ht, int key) {
-  int ll = key % (SIZE-1);
-  if ((*ht)[ll] != NULL) {
-      node * n = linked_list_find((*ht)[ll], &key);
+hnode * hash_table_search(hash_table ht, int key) {
+  int ll = key % (HASH_TABLE_SIZE-1);
+  if (ht[ll] != NULL) {
+      node * n = linked_list_find(ht[ll], &key);
       if (n != NULL) {
           return (hnode *)(n->data);
       }
@@ -46,23 +51,24 @@ hnode * hash_table_search(hash_table * ht, int key) {
   return NULL;
 }
 
-void hash_table_delete(hash_table * ht, int key) {
-    int ll = key % (SIZE-1);
-    if ((*ht)[ll] != NULL) {
-        node * n = linked_list_find((*ht)[ll], &key);
+void hash_table_delete(hash_table ht, int key) {
+    int ll = key % (HASH_TABLE_SIZE-1);
+    if (ht[ll] != NULL) {
+        node * n = linked_list_find(ht[ll], &key);
         if (n != NULL) {
-            linked_list_delete((*ht)[ll], n);
+            linked_list_delete(ht[ll], n);
         }
     }
 }
 
-void hash_table_print(hash_table * ht) {
-  for (int i = 0; i < SIZE; i++) {
-    if ((*ht)[i] != NULL) {
+void hash_table_print(hash_table ht) {
+  for (int i = 0; i < HASH_TABLE_SIZE; i++) {
+    if (ht[i] != NULL) {
         printf("--++--\n");
-        printf("len: %d\n", ((linked_list *)((*ht)[i]))->length);
-        printf("%p\n", (*ht)[i]);
-        linked_list_print((*ht)[i]);
+        printf("len: %d\n", ((linked_list *)(ht[i]))->length);
+        printf("%p\n", ht[i]);
+        linked_list_print(ht[i]);
     }
   }
 }
+#endif
--- hash_table/hash_table_ll.h
+++ hash_table/hash_table_ll.h
@@ -4,6 +4,7 @@
 
 #include "./hash_table_ll.c"
 
-void hash_table_insert(hash_table * ht, void * d, int key);
-hnode * hash_table_search(hash_table * ht, int key);
-void hash_table_delete(hash_table * ht, int key);
+hash_table hash_table_new();
+void hash_table_insert(hash_table ht, void * d, int key);
+hnode * hash_table_search(hash_table ht, int key);
+void hash_table_delete(hash_table ht, int key);
